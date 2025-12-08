649cdff15c40b02ce2b245a1f524fbd530d64d75
colin <cwcnsa@gmail.com>
2025-12-08 12:39:16 -0700

    frontend skeleton

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutPage } from './pages/about-page/about-page';
+import { MySightingsPage } from './pages/my-sightings-page/my-sightings-page';
+import { ExplorePage } from './pages/explore-page/explore-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'explore', pathMatch: 'full' },
+    { path: 'explore', component: ExplorePage },
+    { path: 'my-sightings', component: MySightingsPage },
+    { path: 'about', component: AboutPage },
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'wildlife_app';
+  protected title = 'wildlife-trails-app';
 }
